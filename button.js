f0e822c3f911e36e640a745e1c751c60c2a8158b
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-09-17 18:52:39 -0500

    adding content

---
+++ button.js
@@ -0,0 +1,28 @@
+var 
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+//var password
+
+
+function genPassword () {
+    //var passwordLength = prompt ("Please select length ")
+}
