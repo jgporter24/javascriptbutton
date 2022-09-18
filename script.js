7f824bd756f103d2dd333dfb31ed08379bdb0dc5
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-09-17 23:21:52 -0500

    content update

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+  var length = Number(prompt("Please enter a length between 8 and 128 characters."));
+  if (length < 8 || length > 128) {
+    window.alert("Must be greater than 8 and less than 128");
+    {return};
+  };
+
+  var lowerCase = confirm("Would you like to include lower case characters?");
+
+  var upperCase = confirm("Would you like to include upper case characters?");
+  var numeric = confirm("Would you like to include numbers?");
+  var specialChar = confirm("Would you like special characters?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,4 +23,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+
+}
+
